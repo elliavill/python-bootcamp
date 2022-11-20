c2de14992b35f73cde1c6b68f317687a71a1ded5
Aurelia Villyani <aureliavillyani@gmail.com>
2022-11-20 02:05:56 -0600

    turtle documentation day 18

---
+++ day16to24/d18-turtel/turtle-challenge2.py
@@ -0,0 +1,11 @@
+# learn how to import modules, install packages, working with alliances
+import turtle as t
+
+tim = t.Turtle()
+
+########### Challenge 2 - Draw a Dashed Line ########
+for _ in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
