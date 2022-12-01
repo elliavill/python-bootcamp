2f314accea7907c16e038cfb3a97abe2f9aeca34
elliavill <aureliavillyani@atlassian.com>
2022-12-01 15:05:59 -0600

    add day 20

---
+++ d20-21-snake-game/main.py
@@ -0,0 +1,62 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+"""
+   STEP 1:
+   # Build the snake body
+   starting_positions = [(0,0), (-20, 0), (0,0)]
+
+   for position in starting_positions:
+       new_segment = Turtle("square")
+       new_segment.color("white")
+       new_segment.goto(position)
+"""
+
+
+screen.exitonclick()
+
+
