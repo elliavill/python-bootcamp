93a0bc53726fc4344ea5fed3e8f4dff20c6ec57d
elliavill <aureliavillyani@atlassian.com>
2022-12-01 15:44:18 -0600

    turtle capstone and mail-merge

---
+++ d23-turtle-capsotne/main.py
@@ -0,0 +1,48 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+"""
+   Steps: How it works
+   1. Move the turtle with keypress
+   2. Create and move the cars
+   3. Detect collision with car
+   4. Detect when turtles reaches the other side
+   5. Create a scoreboard
+"""
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    #Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    #Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+
+screen.exitonclick()
